3b10467d07a398d658ea82a4fddfa9c6d8cdda22
jarilton <jariltonpereira02@hotmail.com>
2021-11-16 20:02:38 -0300

    Implementando interface gráfica

---
+++ src/components/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+      <v-container fluid>
+          <div class="pills">
+              <Pill v-for="word in groupedWords" :key="word.name"
+                :name="word.name" :amount="word.amount" />
+          </div>
+      </v-container>
+  </div>
+</template>
+
+<script>
+import Pill from './Pill.vue'
+
+export default {
+    components: { Pill },
+
+    data: function() {
+        return {
+            groupedWords: [
+                { name: 'i', amount: 1234},
+                { name: 'you', amount: 900},
+                { name: 'he', amount: 853},
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
