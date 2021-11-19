3321ec5c0715696c9fc5ef91b1667497d478799e
jarilton <jariltonpereira02@hotmail.com>
2021-11-18 22:36:51 -0300

    Ajuestes do complemento de arquivos

--- src/backend/index.js
+++ src/backend/index.js
@@ -2,21 +2,14 @@ const { ipcMain } = require('electron')
 
 const pathsToRows = require('./pathsToRows')
 const prepareData = require('./prepareData')
+const groupWords = require('./groupWords')
 
 ipcMain.on('process-subtitles', (event, paths) => {
 
     pathsToRows(paths)
         .then(rows => prepareData(rows))
-        .then(words => console.log(words))
-        .then(() => {
-            event.reply('process-subtitles', [
-                { name: 'i', amount: 1234 },
-                { name: 'you', amount: 900 },
-                { name: 'he', amount: 853 },
-                { name: 'she', amount: 853 },
-                { name: 'our', amount: 133 },
-                { name: 'house', amount: 23 },
-            ])
-        })
+        .then(words => groupWords(words))
+        .then(groupedWords => event.reply('process-subtitles', groupedWords)
+        )
 
 })
